b8e64768f39d0befba7ecbfd763b6881155f79a9
balakrishna <balakrishna@balakrishnas-MacBook-Pro.local>
2023-08-13 13:37:05 +0530

    fixed home navigation

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/trify_admin",
+  base: "/trify_admin/",
   plugins: [react()],
 });
