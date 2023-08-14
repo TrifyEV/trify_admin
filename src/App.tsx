c4feaf8b2adcb8e21cb17147c8070e798dfed262
balakrishna <balakrishna@balakrishnas-MacBook-Pro.local>
2023-08-14 18:58:01 +0530

    added missing assects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { LazyRoutes } from "./Components/Routes";
 import Navbar from "./Components/common/Navbar";
 import { useAuth } from "./Components/common/AuthProvider";
 import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
+import "leaflet/dist/images/marker-shadow.png";
 
 function App() {
   const auth = useAuth();
